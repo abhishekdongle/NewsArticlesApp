bc7e21e018d83507b7b8d590aa5abf956ce18efe
abhishekdongle <abhidongle04@gmail.com>
2025-04-28 15:38:38 +0530

    dev: added ArticlesUseCase class and calling api from ArticlesViewModel

---
+++ shared/src/commonMain/kotlin/com/abhishek/dongle/newsarticlesapp/article/ArticlesUseCase.kt
@@ -0,0 +1,40 @@
+package com.abhishek.dongle.newsarticlesapp.article
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.math.abs
+
+class ArticlesUseCase(private val articlesService: ArticlesService) {
+
+    suspend fun getArticles(): List<Article> {
+        val articlesRaw = articlesService.fetchArticles()
+        return mapArticles(articlesRaw)
+    }
+
+    private fun mapArticles(articlesRaw: List<ArticleRaw>) = articlesRaw.map { raw ->
+        Article(
+            title = raw.title,
+            desc = raw.desc ?: "Upcoming details",
+            date = getDaysAgo(raw.date),
+            imageUrl = raw.imageUrl
+                ?: "https://techcrunch.com/wp-content/uploads/2024/05/Minecraft-keyart.jpg?resize=1200,720"
+        )
+    }
+
+    private fun getDaysAgo(date: String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
+
+        return when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+    }
+}
